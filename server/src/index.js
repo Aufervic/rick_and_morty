b85b6e5fa326cc788c834ea8b1270fce17a00466
Aufer Victoriano <aufer.vicc@gmail.com>
2023-05-04 08:43:16 -0500

    Faltó línea que sincroniza sequelize con la BD

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const {conn} = require('./DB_connection')
 const server = require('./app')
 
-
 const PORT = process.env.PORT || 3001
 
+conn.sync({alter: true})
+.then(()=>{
 
-server.listen(PORT, ()=>{
-  console.log(`Server listening on PORT: ${PORT}`)
+  server.listen(PORT, ()=>{
+    console.log(`Server listening on PORT: ${PORT}`)
+  })
 })
+
